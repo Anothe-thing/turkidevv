30646555b5ab951ef75f326ccb457716bc293269
Anothe-thing <79436309+Anothe-thing@users.noreply.github.com>
2022-06-03 09:02:32 +0300

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,11 @@
+let ItemsData = [
+  {
+    id: "LOLy",
+    name: "bot dis.",
+    price: 45,
+    desc: "no nono",
+    img: "images/img-1.jpg",
+    link: "https://replit.com/~",
+    namebut: "test"
+  },
+];
